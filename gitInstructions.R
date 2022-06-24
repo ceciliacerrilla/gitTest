413ed74167a919f20635df01dcf2f72ecec03df2
ceciliacerrilla <cecilia.cerrilla@gmail.com>
2022-06-24 14:29:54 +0200

    Commit 1

---
+++ gitInstructions.R
@@ -0,0 +1,6 @@
+
+
+# How to revert to a previous version of your code:
+# In Terminal window (not console or script), type:
+  # git reset --hard SHA
+    # SHA is found at: Git tab > History > Desired commit 
